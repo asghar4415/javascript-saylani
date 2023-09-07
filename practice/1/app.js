eb20c31a29615dd16421b527a6ce05bcbd70b4ec
Asghar Ali <asgharali.224415@gmail.com>
2023-09-07 21:57:45 +0500

    day 2 updated

--- practice/1/app.js
+++ practice/1/app.js
@@ -13,3 +13,24 @@ var username= "asghar"
 
 console.log(username)
 
+var num1=100
+var num2=200
+
+console.log(num1+num2)      // addition
+console.log("100"+"200")     // string concatenation
+
+
+// Data Types:
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+
+var name="asghar"    // string
+var age=20      // int, float, double
+var isFeePaid=true     // boolean
+var fee=null       // null
+var abc            // undefined
+
+
