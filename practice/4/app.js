fce37f82bb1847b29c65b73df5a5639ced215907
Asghar Ali <asgharali.224415@gmail.com>
2023-09-14 22:57:38 +0500

    day 6

--- practice/4/app.js
+++ practice/4/app.js
@@ -1,13 +1,11 @@
-var age= +prompt("What is your age?", "19")  
-if (age >= 18)
-{
+var age = +prompt("What is your age?", "19")
+if (age >= 18) {
     alert("You are eligible to vote.")
     console.log("Age: ", age)
 }
-else
-{
+else {
     alert("You are not eligible to vote.")
-console.log("Age: ", age)
+    console.log("Age: ", age)
 }
 // conditional statements are used to perform different actions based on different conditions. 
 
---
+++ practice/5/app.js
@@ -0,0 +1,78 @@
+    // = is assignment operator
+    // == is comparison operator
+    // === is strict comparison operator used to compare data type and value
+
+    // var stdAge = prompt("Enter your age: ")
+
+    // if(stdAge == 20)
+    // {
+    //     console.log("Age is 20");
+    // }
+    // else if(stdAge == 21)
+    // {
+    //     console.log("Age is 21");
+    // }
+    // else
+    // {
+    //     console.log("Age is not 20");
+    // }
+
+    // if(stdAge >=18)
+    // {
+    //     console.log("The person is an adult")
+    // }
+    // else if(stdAge >= 13 && stdAge <= 17)
+    // {
+    //     console.log("The person is a teenager")
+    // }
+    // else if(stdAge >= 6 && stdAge <= 12)
+    // {
+    //     console.log("The person is a child")
+    // }
+    // else
+    // {
+    //     console.log("The person is dead.")
+    // }
+
+    // var num1= prompt("Enter first number: ");
+    // var num2= prompt("Enter second number: ");
+    // var op= prompt("Enter operator: ");
+
+    // if(op == "+")
+    // {
+    //     console.log(num1 + num2);
+    // }
+    // else if(op == "-")
+    // {
+    //     console.log(num1 - num2);
+    // }
+    // else if(op == "*")
+    // {
+    //     console.log(num1 * num2);
+    // }
+    // else if(op == "/")
+    // {
+    //     console.log(num1 / num2);
+    // }
+    // else
+    // {
+    //     console.log("Invalid operator");
+    // }
+
+    var num1= prompt("Enter first number: ");
+    var num2= prompt("Enter second number: ");
+
+    if(num1 > num2)
+    {
+        console.log(num1 + " is greater than " + num2);
+    }
+    else if(num1 < num2)
+    {
+        console.log(num1 + " is less than " + num2);
+    }
+    else
+    {
+        console.log(num1 + " is equal to " + num2);
+    }
+
+    
