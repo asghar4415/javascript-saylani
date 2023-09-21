a644f3adf05d2afac683c4cf4c6183289e6fb5e2
Asghar Ali <asgharali.224415@gmail.com>
2023-09-21 22:02:17 +0500

    day 9

---
+++ practice/6/app.js
@@ -0,0 +1,29 @@
+//Arrays in js are dynamic in nature i.e. they can grow or shrink in size as per the requirement of the program. 
+//There are two ways to create an array in js:
+//1. Using array literal
+//2. Using array constructor
+
+//1. Using array literal
+
+// var arr = [1,2,3,4,5];
+// console.log(arr);
+
+// var arr1= ["Hello", 1, true, 1.1, undefined, null, {name: "Rahul"}, [1,2,3,4,5],"Bye"];
+// console.log(arr1);
+
+var arr= [[2,3,5,2], [1,2,3,4]];
+console.log(arr);
+console.log(arr[0][2]);
+
+//2. Using array constructor
+
+// var arr = new Array(1,2,3,4,5);
+// console.log(arr);
+
+// var arr = new Array(5);
+// console.log(arr);
+
+
+
+
+
