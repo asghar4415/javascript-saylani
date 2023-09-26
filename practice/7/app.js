e3e2aed3668852f224e6baee68143bcc976db649
Asghar Ali <asgharali.224415@gmail.com>
2023-09-26 21:34:17 +0500

    updated

---
+++ practice/7/app.js
@@ -0,0 +1,73 @@
+//pop, push method
+
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr);
+// arr.push(11);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// push method adds an element to the end of an array
+// pop method removes an element from the end of an array
+
+// Slice method
+
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr);
+// console.log(arr.slice(2,5));
+// console.log(arr.slice(2));
+// console.log(arr.slice(2,-2));
+
+// slice method returns a new array. it takes two parameters. first one is the starting index and the second one is the ending index. if the second parameter is not given, it will slice till the end of the array. if the second parameter is negative, it will slice from the end of the array.
+
+// Splice method
+
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr);
+// console.log(arr.splice(2,5));
+// console.log(arr);
+// console.log(arr.splice(2));
+// console.log(arr);
+// console.log(arr.splice(2,-2));
+// console.log(arr);
+
+// splice method returns the removed elements. it takes two parameters. first one is the starting index and the second one is the number of elements to be removed. if the second parameter is not given, it will remove till the end of the array. if the second parameter is negative, it will remove from the end of the array.
+
+// Shift method
+
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr);
+// console.log(arr.shift());
+// console.log(arr);
+// console.log(arr.shift());
+// console.log(arr);
+
+
+// shift method removes an element from the beginning of an array
+
+// Unshift method
+
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr);
+// console.log(arr.unshift(11));
+// console.log(arr);
+// console.log(arr.unshift(12));
+// console.log(arr);
+
+// unshift method adds an element to the beginning of an array
+
+// Concat method
+
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// var arr2 = [11,12,13,14,15];
+// console.log(arr);
+// console.log(arr2);
+// console.log(arr.concat(arr2));
+// console.log(arr);
+// console.log(arr2);
+
+// concat method returns a new array. it takes an array as a parameter and adds it to the end of the array.
