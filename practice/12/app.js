7ed66cceba94a3c2394265f4b5dcf239c22cf258
Asghar Ali <asgharali.224415@gmail.com>
2023-10-17 22:23:43 +0500

    day 13

---
+++ practice/12/app.js
@@ -0,0 +1,29 @@
+// var now = new Date()
+// console.log(now)
+
+// var offerenddate= new Date("oct 24 2023")
+// console.log(offerenddate)
+
+// var nowms= now.getTime()
+// var offerms= offerenddate.getTime()
+// console.log(nowms)
+// console.log(offerms)
+
+// var diff= offerms-nowms
+// console.log(diff)
+
+// var remainingdays= Math.floor(diff / (1000 * 60 * 60* 24 )) 
+// console.log("Remaining days: ", remainingdays)
+
+//Functions
+
+function hello()    //function declaration
+{
+    console.log("hello")
+}
+
+hello()   //function call 
+hello()
+hello()
+hello()
+
---
+++ practice/13/app.js
@@ -0,0 +1,12 @@
+//functions continued.....
+
+
+// function add(a,b){
+//     var c= parseInt(a)+parseInt(b);
+//     document.write("Addition is: "+c+"<br>");
+// }
+// add(2,3);
+// add(4,5);
+// add(6,7);
+
+
