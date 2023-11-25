50d6fa30ae87a45f408c6e9753dcc28023e727be
Asghar Ali <asgharali.224415@gmail.com>
2023-11-25 21:12:09 +0500

    updated

---
+++ practice/24/app.js
@@ -0,0 +1,13 @@
+// objects methods
+
+var person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    credentials: function () {
+        console.log(this.firstName + " " + this.lastName);
+        console.log("ID: " + this.id);
+    }
+};
+
+person.credentials();
