1a55882c62b1bd8ec944129c5d4f911601b5d31d
Asghar Ali <asgharali.224415@gmail.com>
2023-12-05 22:33:10 +0500

    updated

---
+++ practice/28/app.js
@@ -0,0 +1,26 @@
+//Storage 
+
+// local storage: data stays until manually cleared out or deleted 
+
+//localStorage.setItem('name','mario');  //set item in local storage
+//localStorage.setItem('age','50');  //set item in local storage
+
+//session storage:  data stays until browser tab is closed
+
+//sessionStorage.setItem('name','mario');  //set item in local storage
+//sessionStorage.setItem('age','50');  //set item in local storage
+
+//remove from storage
+
+//localStorage.removeItem('name');
+
+//get data from storage
+
+//let name = localStorage.getItem('name');
+//let age = localStorage.getItem('age');
+
+//clear local storage
+
+//localStorage.clear();
+
+//console.log(name,age);
