213db405da14af96de05691279f891dc84c799af
Asghar Ali <asgharali.224415@gmail.com>
2024-01-20 21:15:01 +0500

    updated

---
+++ advanced/5/app.js
@@ -0,0 +1,12 @@
+// import export
+
+var arr = [1, 2, 3, 4, 5];
+var obj = {
+    name: 'zhangsan',
+    age: 18
+};
+console.log(arr);
+console.log(obj);
+
+
+export  { arr, obj }; 
---
+++ advanced/5/app1.js
@@ -0,0 +1,7 @@
+
+import {obj} from './app.js';
+import * as namess from "./app.js";
+
+console.log(obj); // {name: "zhangsan", age: 18}
+
+console.log(namess); // {arr: Array(5), obj: {…}, __esModule: true, default: {…}}
